bd067f3e0bf23962992d773644c8046acac9ac66
whutao <whutao.dev@gmail.com>
2022-12-30 00:33:32 +0300

    Fix.

--- Sources/SugarKit/Layout/UIView+swizzle.swift
+++ Sources/SugarKit/Layout/UIView+swizzle.swift
@@ -34,9 +34,9 @@ import UIKit
 extension UIView {
     
     
-    /// Custom implementation of `didMoveToWindow` method that applies pending autolayout constraints.
-    @objc internal func _didMoveToWindow() {
-		if !(window == nil), !(constraintDescriptionBag.isEmpty) {
+	/// Custom implementation of `didMoveToSuperview` method that applies pending autolayout constraints.
+    @objc internal func _didMoveToSuperview() {
+		if !(constraintDescriptionBag.isEmpty) {
             activate(constraintDescriptionBag)
             constraintDescriptionBag = []
         }
@@ -45,8 +45,8 @@ extension UIView {
     /// Swizzles the `didMoveToSuperview` method implementation.
     internal static let swizzleDidMoveToSuperviewImplementation: Void = {
        
-        let aSelector: Selector = #selector(UIView.didMoveToWindow)
-        let swizzledSelector: Selector = #selector(UIView._didMoveToWindow)
+        let aSelector: Selector = #selector(UIView.didMoveToSuperview)
+        let swizzledSelector: Selector = #selector(UIView._didMoveToSuperview)
         
         let aMethod: Method? = class_getInstanceMethod(UIView.self, aSelector)
         let swizzledMethod: Method? = class_getInstanceMethod(UIView.self, swizzledSelector)
